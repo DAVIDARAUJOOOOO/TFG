09113a0ee545bb799f1f9a56b476ae23e8825d75
DAVIDARAUJOOOOO <davidaraujogarcia12@gmail.com>
2026-06-07 21:18:19 +0200

    fix: scaler

--- src/training/training_mlp/mlp_preprocessing.py
+++ src/training/training_mlp/mlp_preprocessing.py
@@ -1,6 +1,7 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from src.training.training_1.smotenc_experiment import apply_smotenc
+import pandas as pd
 
 def prepare_mlp_data(X_train, y_train, X_test, categorical_columns):
 
@@ -16,9 +17,23 @@ def prepare_mlp_data(X_train, y_train, X_test, categorical_columns):
     # scaling
     scaler = StandardScaler()
 
-    X_tr = scaler.fit_transform(X_tr)
-    X_val = scaler.transform(X_val)
-    X_test_scaled = scaler.transform(X_test)
+    X_tr = pd.DataFrame(
+        scaler.fit_transform(X_tr),
+        columns=X_train.columns,
+        index=X_tr.index
+    )
+
+    X_val = pd.DataFrame(
+        scaler.transform(X_val),
+        columns=X_train.columns,
+        index=X_val.index
+    )
+
+    X_test_scaled = pd.DataFrame(
+        scaler.transform(X_test),
+        columns=X_train.columns,
+        index=X_test.index
+    )
 
     # SMOTE
     X_tr_res, y_tr_res = apply_smotenc(X_tr, y_tr, categorical_columns)
